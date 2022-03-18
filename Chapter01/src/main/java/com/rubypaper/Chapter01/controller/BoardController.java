add7d9b65466e2120d86ad583fb2497324dff0c6
tseop <77163842+tseop@users.noreply.github.com>
2022-03-18 14:53:07 +0900

    Chapter01 END
    Chapter01 END

---
+++ Chapter01/src/main/java/com/rubypaper/Chapter01/controller/BoardController.java
@@ -0,0 +1,55 @@
+package com.rubypaper.Chapter01.controller;
+
+import com.rubypaper.Chapter01.domain.BoardVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
+@RestController
+public class BoardController {
+
+    public BoardController() {
+        System.out.println("===> BoardController 생성");
+    }
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello : " + name;
+    }
+
+    // View 없이 return 을 하면 JSON 데이터로 화면에 출력이 된다.
+    @GetMapping("/getBoard")
+    public BoardVO getBoard() {
+        BoardVO board = new BoardVO();
+
+        board.setSeq(1);
+        board.setTitle("테스트 제목..");
+        board.setWriter("테스터");
+        board.setContent("테스트 내용......");
+        board.setCreateDate(new Date());
+        board.setCnt(0);
+
+        return board;
+    }
+
+    @GetMapping("/getBoardList")
+    public ArrayList<BoardVO> getBoardList() {
+        ArrayList<BoardVO> list = new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            BoardVO board = new BoardVO();
+            board.setSeq(i);
+            board.setTitle("테스트 제목..");
+            board.setWriter("테스터");
+            board.setContent("테스트 내용......");
+            board.setCreateDate(new Date());
+            board.setCnt(0);
+
+            list.add(board);
+        }
+        return list;
+    }
+}
