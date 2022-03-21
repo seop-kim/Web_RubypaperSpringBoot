70f156544b626161f52d7e0d37cc34e109c376b0
tseop <77163842+tseop@users.noreply.github.com>
2022-03-21 16:02:47 +0900

    Chapter2 end
    chapter2 end

--- Chapter02/src/main/java/com/example/Chapter02/Chapter02Application.java
+++ Chapter02/src/main/java/com/example/Chapter02/Chapter02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Chapter02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Chapter02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter02Application.class, args);
+    }
 
 }
